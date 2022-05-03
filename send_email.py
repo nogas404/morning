8a4f53e70afe50102ee75ca2fb51d9b4728c0531
Patryk <patryk_git@outlook.com>
2022-05-03 13:06:38 +0200

    sending emails functionality is now completed

---
+++ send_email.py
@@ -0,0 +1,45 @@
+# really helpful link https://www.codeitbro.com/send-email-using-python/    (WARNING python2)
+# docs: https://devdocs.io/python/library/smtplib
+# and big thanks to this guy https://stackoverflow.com/a/58997830
+
+def send_email(text):
+    import os
+
+    # modify those variables in github actions secrets
+    SENDER_EMAIL = os.environ['SENDER_EMAIL']
+    SENDER_PASSWORD = os.environ['SENDER_PASSWORD']
+    RECIVER_EMAIL = os.environ['RECIVER_EMAIL']
+    
+    
+    from email.message import EmailMessage
+    from datetime import date
+
+    # email formatting
+    new_Message = EmailMessage()
+    new_Message['Subject'] = f'Daily Newsletter {date.today().strftime("%d/%m")}'
+    new_Message['From'] = SENDER_EMAIL
+    new_Message['To'] = RECIVER_EMAIL
+    new_Message.set_content(text)
+    
+    
+    
+    files = ['codeitbro-logo.png', 'cat.gif']
+    # add images to email
+    files = ['cases.jpeg', 'covid_tests.jpeg']
+    for file in files:
+        with open(file, 'rb') as f:
+            image_data = f.read()
+            image_name = f.name
+        new_Message.add_attachment(image_data, maintype='image', subtype='jpeg', filename=image_name)
+
+    # authenticate and send email
+    from smtplib import SMTP
+    import ssl
+    context=ssl.create_default_context()
+    with SMTP('smtp.mail.yahoo.com', port=587) as smtp:
+        smtp.starttls(context=context)
+        smtp.login(SENDER_EMAIL, SENDER_PASSWORD)
+        smtp.send_message(new_Message)
+
+if __name__ == '__main__':
+    send_email('hi')
